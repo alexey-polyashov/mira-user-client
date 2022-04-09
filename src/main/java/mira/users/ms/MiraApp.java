cbe0ac98084338058a1daf5e22ed48d2c04f1c09
alexey-polyashov <polyashofff@yandex.ru>
2022-04-09 21:03:08 +0300

    Initial commit

---
+++ src/main/java/mira/users/ms/MiraApp.java
@@ -0,0 +1,11 @@
+package mira.users.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MiraApp.class, args);
+    }
+}
